185d8c20a06f9d9f7ba027a56341f70986139611
ilvxna <ilvxna@hotmail.com>
2019-11-28 19:15:57 +0800

    add sqlist

--- DataStructure/plus.cpp
+++ DataStructure/plus.cpp
@@ -9,5 +9,8 @@ int plus1(int n1, int n2) {
 TEST(TestPlus, TestNamePlus) {
 	EXPECT_EQ(2, plus1(1, 2));
 	cout << "顺序表初始化..." << endl;
+	int choose = -1;
+	cout << "请选择..." << endl;
+	cin >> choose;
 	EXPECT_TRUE(true);
 }
